eaef580f9aae60dd1ade2171c39b80bd73e994f1
ManamiMayoki <manamimarufa@gmail.com>
2026-09-05 11:56:37 +0600

    exercise

--- beginner/learner.r
+++ beginner/learner.r
@@ -171,4 +171,19 @@ mean(marks)
 mean(marks, na.rm = TRUE) #mean(marks, na.rm = TRUE)
 
 
-#
+#16. Practice Project — Student Marks
+students <- c("A", "B", "C", "D", "E")
+marks <- c(80, 65, 90, 75, 88)
+# Find average
+# Highest mark
+# Lowest mark
+# Student C's mark
+# Add 5 bonus marks
+# Students who scored above 80
+mean(marks)
+max(marks)
+min(marks)
+marks[3]
+marks + 5
+students[marks > 80] #FALSE FALSE TRUE FALSE TRUE (logical vector) → C, E
+
---
+++ exercises/student_exercises.R
@@ -0,0 +1,74 @@
+# Exercise 1 — Variables
+
+# Create:
+
+# name
+# age
+# department
+# semester
+# cgpa
+
+# Print them.
+
+#--------------------------------------------------------------------------------
+
+# Exercise 2 — Arithmetic
+
+# Calculate:
+
+# 25 + 17
+# 100 - 37
+# 12 * 8
+# 144 / 12
+# 2^10
+# 100 %% 7
+
+#--------------------------------------------------------------------------------
+
+# Exercise 3 — Vector
+
+# Create:
+
+# temperature <- c(31, 33, 30, 29, 32, 34, 31)
+
+# Find:
+
+# Mean
+# Median
+# Maximum
+# Minimum
+# Standard deviation
+# Number of observations
+
+#--------------------------------------------------------------------------------
+
+# Exercise 4 — Filtering
+
+# Using:
+
+# marks <- c(45, 67, 89, 32, 76, 91, 55)
+
+# Find:
+
+# Marks greater than 60
+# Marks less than 50
+# Marks equal to 91
+# Number of students scoring above 70
+
+
+#--------------------------------------------------------------------------------
+
+# Exercise 5 — Research-style problem
+
+# Suppose you collected the ages of 10 participants:
+
+# age <- c(19, 21, 20, 22, 25, 19, 23, 21, 20, 24)
+
+# Calculate:
+
+# Mean
+# Median
+# Minimum
+# Maximum
+# Variance
+# Standard deviation
